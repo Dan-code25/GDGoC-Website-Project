e931d1c2a7b46474a6dbf2c2ad53cc3c86859901
Wen <frondairwen@gmail.com>
2025-11-04 20:22:45 +0800

    exitBtn Added

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -50,13 +50,24 @@ function Profile({ onClose , onSignOut, onChangePassword, isClosing}){
         >
             <div className="profile-container" onClick={(e) => e.stopPropagation()}>
                 <AnimationBackground />
-                
+
+                {/* <span className="exitBtn"></span> */}
                 <button className='ph--triangle-fill' onClick={handleExit}></button>
 
-                <div className='logOut profile' onClick={handleSignOut} role='button'>
+                {/* <div className='logOut profile' onClick={handleSignOut} role='button'>
                     <span className="logoutText"></span> 
                     <span className="logoutIcon"></span>
+                </div> */}
+
+                {/* header actions bar */}
+                <div className="profile-actions">
+                    <span className="exitBtn" onClick={handleExit} />
+                    <div className="logOut profile" onClick={handleSignOut} role="button">
+                    <span className="logoutText"></span>
+                    <span className="logoutIcon"></span>
+                    </div>
                 </div>
+                
 
                 <div className="name">
                     <h4>
