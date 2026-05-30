a35fae796b55d4cecbce362c2fabe319d967aea9
SPlinterRed <131634018+SPlinterRed@users.noreply.github.com>
2026-05-30 14:30:03 +0800

    Refactor code structure for improved readability and maintainability

--- frontend/src/assets/images/team-images/EB/EBimages.js
+++ frontend/src/assets/images/team-images/EB/EBimages.js
@@ -1,16 +1,16 @@
 import Reyes from './Alfred_reyes.svg';
 import Torion from './Clifford Roy E Torion_Chief Innovations Officer.png';
-import Ropeta from './Hannah Lorraine Ropeta_Executive Secretary.png';
+import Ropeta from './Hannah Lorraine Ropeta, 2025-2026, Executive Secretary.png';
 import Padua from './Jefferson Padua_Chief Finance Officer.png';
 import Abas from './Jhondel Mico Abas_ Co-Organizer .png';
 import Pantalunan from './Josh Pantalunan_Chief Administrative Officer.png';
 import Tabuzo from './Jude S. Tabuzo Jr._Chief Operations Officer.png';
 import Serapio from './Kien Leriss R. Serapio_Organizer.png';
 import Renacido from './Luke Joseph Renacido_Chief Communications Officer - Page.png';
-import Santos from './Maria Leonabelle Santos_Chief Relations Officer.png';
-import Maisog from './Mitch Angela Maisog_Googlet Head.png';
-import Ledres from './Racy Jhanna D. Ledres_Chief Human Resources Officer .png';
-import Zendy from './Zendy Santos_Chief Communications Officer - Events.png';
+import Santos from './Maria Leonabelle Santos, 2025-2026, Chief Relations Officer.jpg';
+import Maisog from './Mitch Angela Maisog, 2025-2026, Googlet Head.png';
+import Ledres from './Racy Jhanna D. Ledres, 2025-2026, Chief Human Resources Officer.png';
+import Zendy from './Zendy Santos, 2025-2026, Chief Communications Officer - Events.png';
 
 //Logo
 import LinkedIn from './LINKEDIN.svg';
